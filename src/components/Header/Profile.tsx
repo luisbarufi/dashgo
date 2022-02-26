5a377026992ff5324c44bca71108171f24b43b90
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-02-26 20:56:24 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,20 +1,26 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({showProfileData = true}:ProfileProps ) {
   return (
     <Flex align="center">
-    <Box mr="4" textAlign="right">
-      <Text>Luis Barufi</Text>
-      <Text color="gray.300" fontSize="small">
-        luisbarufi@gmail.com
-      </Text>
-    </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Luis Barufi</Text>
+          <Text color="gray.300" fontSize="small">
+            luisbarufi@gmail.com
+          </Text>
+        </Box>
+      )}
 
-    <Avatar
-      size="md"
-      name="Luis Barufi"
-      src="https://github.com/luisbarufi.png"
-    />
-  </Flex>
+      <Avatar
+        size="md"
+        name="Luis Barufi"
+        src="https://github.com/luisbarufi.png"
+      />
+    </Flex>
   );
 }
