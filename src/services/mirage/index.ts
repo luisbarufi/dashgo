394ea143ca1c48e6cc5ff509d95099507ec4ca0b
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-03-18 23:49:27 -0300

    pagination logic

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 import { faker } from '@faker-js/faker';
 
 type User = {
@@ -28,13 +28,31 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList('user', 10);
+      server.createList('user', 200);
     },
 
     routes() {
       this.namespace = 'api';
       this.timing = 755;
-      this.get('/users');
+
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.params;
+
+        const total = schema.all('user').length
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pagedEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user'))
+          .users.slice(pageStart, pagedEnd);
+
+          return new Response(
+            200,
+            { 'x-total-count': String(total)},
+            { users }
+          )
+      });
+
       this.post('/users');
       this.namespace = '';
       this.passthrough();
